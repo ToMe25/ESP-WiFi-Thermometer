93163ed48c676a7a646eab7eee5d909042d50736
--global <--global>
2023-08-21 10:41:41 +0200

    Add custom get_msb function to TrackingFallbackHandler
    Because the std log2 function isn't available on arm hosts for some reason

--- src/AsyncTrackingFallbackWebHandler.cpp
+++ src/AsyncTrackingFallbackWebHandler.cpp
@@ -10,12 +10,26 @@
 #if ENABLE_PROMETHEUS_SCRAPE_SUPPORT == 1 || ENABLE_PROMETHEUS_PUSH == 1
 #include "prometheus.h"
 #endif
-#include <cmath>
+
+/**
+ * A method to get the index of the Most Significant Bit set in a number.
+ * Necessary because the std log2 function doesn't exist on arm hosts.
+ *
+ * @param number	The number to get the MSB for.
+ * @return	The index of the Most Significant Bit set in the number.
+ */
+uint8_t get_msb(const uint32_t number) {
+	uint8_t idx = 0;
+	while (number >> (idx + 1)) {
+		idx++;
+	}
+	return idx;
+}
 
 web::AsyncTrackingFallbackWebHandler::AsyncTrackingFallbackWebHandler(
 		const String &uri, HTTPFallbackRequestHandler fallback) :
 		_uri(uri), _fallbackHandler(fallback), _handlers(
-				(size_t) std::log2((size_t) HTTP_ANY) + 1) {
+				get_msb(HTTP_ANY) + 1) {
 
 }
 
@@ -24,12 +38,7 @@ web::AsyncTrackingFallbackWebHandler::~AsyncTrackingFallbackWebHandler() {
 }
 
 web::HTTPRequestHandler web::AsyncTrackingFallbackWebHandler::_getHandler(const WebRequestMethod method) const {
-	WebRequestMethodComposite mtd = method;
-	unsigned idx = 0;
-	while (mtd >>= 1) {
-	    idx++;
-	}
-	return _handlers[idx];
+	return _handlers[get_msb((WebRequestMethodComposite) method)];
 }
 
 void web::AsyncTrackingFallbackWebHandler::setHandler(const WebRequestMethodComposite methods, HTTPRequestHandler handler) {
